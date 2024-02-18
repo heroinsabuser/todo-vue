7d87fd9d66695c3144eb0ff105f53e0c0e7df2b6
heroinsabuser <maksim-kozlov-2002@mail.ru>
2024-02-18 18:25:29 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<script setup lang="ts">
+import { onMounted, ref, type Ref } from "vue";
+
+import MyTodo from "./components/MyTodo.vue";
+
+interface ITodo {
+  id: number;
+  text: string;
+  checked: boolean;
+}
+
+const todos: Ref<ITodo[]> = ref([]);
+
+const text = ref("");
+
+const updateStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos.value));
+};
+
+const addTodo = () => {
+  todos.value.push({
+    id: Math.random(),
+    text: text.value,
+    checked: false,
+  });
+  text.value = "";
+  updateStorage();
+};
+
+const removeTodo = (id: number) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+  updateStorage();
+};
+
+const checkTodo = (id: number) => {
+  todos.value = todos.value.map((todo) => {
+    if (todo.id === id) {
+      todo.checked = !todo.checked;
+    }
+    return todo;
+  });
+  updateStorage();
+};
+
+onMounted(() => {
+  todos.value = JSON.parse(localStorage.getItem("todos") || "[]");
+});
+</script>
+
+<template>
+  <div class="container">
+    <div class="window">
+      <div class="content">
+        <div class="title">
+          <h1>Todo App</h1>
+          <div class="input-content">
+            <input
+              v-model="text"
+              class="input"
+              type="text"
+              placeholder="Добавить задачу"
+              @keyup.enter="addTodo"
+              maxlength="32"
+            />
+            <img
+              @click="addTodo"
+              class="plus"
+              v-if="text"
+              src="/input-plus.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="todos" v-auto-animate>
+          <MyTodo
+            v-for="todo in todos"
+            :key="todo.id"
+            :text="todo.text"
+            :checked="todo.checked"
+            @onClickCheck="checkTodo(todo.id)"
+            @onClickDelete="removeTodo(todo.id)"
+          />
+          <span v-if="todos.length === 0" class="empty">Список дел пуст</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.empty {
+  font-weight: normal;
+  font-size: 14px;
+  margin: 15px 0;
+  text-align: center;
+}
+.plus {
+  position: absolute;
+  right: 0px;
+  cursor: pointer;
+  opacity: 0.5;
+  transition: 0.5s;
+}
+.plus:hover {
+  opacity: 1;
+}
+.input-content {
+  position: relative;
+  margin-bottom: 15px;
+}
+.input {
+  padding-left: 10px;
+  padding-right: 45px;
+  height: 33px;
+  width: 250px;
+  outline: none;
+  background-color: var(--soft-color);
+  border: 1px solid var(--secondary-color);
+  border-radius: 5px;
+}
+.todos {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  overflow-y: auto;
+  max-height: 400px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  background-color: var(--primary-color);
+  min-height: 100dvh;
+  max-height: 100%;
+  width: 100vw;
+}
+.window {
+  background-color: #fff;
+  padding: 20px 50px 30px;
+  margin: 50px 20px;
+  min-width: 350px;
+  width: 600px;
+  border-radius: 20px;
+  box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.2);
+  height: fit-content;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  h1 {
+    font-size: 40px;
+    font-weight: normal;
+    margin-bottom: 10px;
+  }
+}
+.title {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: 2px solid var(--secondary-color);
+  margin-bottom: 15px;
+}
+</style>
---
+++ src/components/MyTodo.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+defineProps({
+  text: String,
+  checked: Boolean,
+});
+const emit = defineEmits(["onClickDelete", "onClickCheck"]);
+</script>
+
+<template>
+  <div class="todo">
+    <div @click="emit('onClickCheck')" class="check">
+      <img v-if="checked" src="/Vector.svg" alt="" />
+    </div>
+
+    <span class="text">{{ text }}</span>
+    <img
+      @click="emit('onClickDelete')"
+      class="delete"
+      src="/close.svg"
+      alt=""
+    />
+  </div>
+</template>
+
+<style scoped>
+.delete {
+  cursor: pointer;
+  margin-right: 10px;
+  width: 24px;
+  height: 24px;
+  opacity: 0.6;
+  transition: 0.5s;
+}
+.delete:hover {
+  opacity: 1;
+}
+.text {
+  text-align: center;
+  flex: 1;
+  font-weight: normal;
+  font-size: 12px;
+}
+.todo {
+  display: flex;
+  align-items: center;
+  background-color: var(--soft-color);
+  border-radius: 5px;
+  width: 100%;
+  min-height: 55px;
+  padding-left: 20px;
+}
+.check {
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  background-color: var(--soft-color);
+  border: 1px solid var(--secondary-color);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.6;
+  transition: 0.5s;
+}
+.check:hover {
+  opacity: 1;
+}
+</style>
